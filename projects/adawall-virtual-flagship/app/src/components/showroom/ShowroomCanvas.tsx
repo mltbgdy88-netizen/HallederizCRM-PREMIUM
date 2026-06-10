f60bec281c7c201c908761f171c63cd47cc18298
mltbgdy88-netizen <mltbgdy88@gmail.com>
2026-06-10 19:15:00 +0300

    ADAWALL app: improve showroom camera and lighting

--- projects/adawall-virtual-flagship/app/src/components/showroom/ShowroomCanvas.tsx
+++ projects/adawall-virtual-flagship/app/src/components/showroom/ShowroomCanvas.tsx
@@ -10,10 +10,13 @@ import { ShowroomScene } from './ShowroomScene';
 export function ShowroomCanvas() {
   return (
     <div className="absolute inset-0">
-      <Canvas camera={{ position: [8, 5, 10], fov: 45 }} shadows>
+      <Canvas camera={{ position: [9, 5.2, 13], fov: 42 }} dpr={[1, 1.7]} shadows>
         <color attach="background" args={["#171412"]} />
-        <ambientLight intensity={0.55} />
-        <directionalLight position={[6, 8, 5]} intensity={1.5} castShadow />
+        <fog attach="fog" args={["#171412", 16, 31]} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[6, 9, 6]} intensity={1.35} castShadow />
+        <pointLight position={[-3.4, 2.8, 1.2]} intensity={2.2} color="#b48a5a" />
+        <pointLight position={[3.4, 2.8, 8.8]} intensity={1.7} color="#f4eee4" />
         <Suspense fallback={null}>
           <ShowroomScene />
           <Environment preset="city" />
