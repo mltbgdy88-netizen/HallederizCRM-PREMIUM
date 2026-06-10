a379b1770ee6054c74d02afdb78baa75c0317b67
mltbgdy88-netizen <mltbgdy88@gmail.com>
2026-06-10 14:04:47 +0300

    ADAWALL app: add showroom scene

---
+++ projects/adawall-virtual-flagship/app/src/components/showroom/ShowroomScene.tsx
@@ -0,0 +1,11 @@
+import { FlagshipShell } from './FlagshipShell';
+import { WallpaperCorridor } from './WallpaperCorridor';
+
+export function ShowroomScene() {
+  return (
+    <group>
+      <FlagshipShell />
+      <WallpaperCorridor />
+    </group>
+  );
+}
